8ba392297b3f48761b1a1a5f723c5d1bf6e17ed4
amkumar072 <amkumar072@gmail.com>
2021-06-24 17:55:58 +0100

    fireabse updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBtNMySjoW0vZPhm1jII5WjxGW3gpv0_cU",
+    authDomain: "madhankumar-poornima.firebaseapp.com",
+    databaseURL: "https://madhankumar-poornima.firebaseio.com",
+    projectId: "madhankumar-poornima",
+    storageBucket: "madhankumar-poornima.appspot.com",
+    messagingSenderId: "748453137",
+    appId: "1:748453137:web:53a616d6a7b1f8f356a0b0",
+    measurementId: "G-EQ2B5QSN2C"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBtNMySjoW0vZPhm1jII5WjxGW3gpv0_cU",
+    authDomain: "madhankumar-poornima.firebaseapp.com",
+    databaseURL: "https://madhankumar-poornima.firebaseio.com",
+    projectId: "madhankumar-poornima",
+    storageBucket: "madhankumar-poornima.appspot.com",
+    messagingSenderId: "748453137",
+    appId: "1:748453137:web:53a616d6a7b1f8f356a0b0",
+    measurementId: "G-EQ2B5QSN2C"
+  }
 };
 
 /*
